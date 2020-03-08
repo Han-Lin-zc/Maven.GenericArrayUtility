c53e91bf7173baf31ebf42e8fd8bf9f51381270b
hlin <Zxsaqw12@&$>
2020-03-08 17:46:51 -0400

    Finish one test class

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,48 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+
+    private T[] arr1;
+    private Integer length;
+
+
+    public ArrayUtility(T[] arr1) {
+        this.arr1 = arr1;
+
+    }
+
+    public T[] mergeArr(T[] arr2) {
+        T[] mergeArr = Arrays.copyOf(this.arr1, this.arr1.length + arr2.length);
+        System.arraycopy(arr2, 0, mergeArr, this.arr1.length, arr2.length);
+        return mergeArr;
+    }
+
+    public Integer countDuplicatesInMerge(T[] arr2, T valueToEvaluate) {
+       T[] newArr = mergeArr(arr2);
+
+        Integer counter = 0;
+        for (T t : newArr) {
+            if (t == valueToEvaluate) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public T[] removeValue(T valueToRemove) {
+        return null;
+    }
+
+    public T getMostCommonFromMerge(T[] arr2) {
+        return null;
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+        return null;
+    }
 }
